20f1bb042d982d13e4232ca406a0305316117661
Benjamin Clark <bennyc@gmail.com>
2012-08-31 17:14:22 -0400

    Add style info & code to also display the snippet below each news item.

--- ajaxExampleScript.js
+++ ajaxExampleScript.js
@@ -22,21 +22,37 @@ function fetchSomeNews(newsRssURL) {
     });
 }
 
-//Now we're talkin. 
+//Now we're talkin. A Jquery-enabled version of the above function, that does a little something
+//extra. 
+//
+//Parameter 'newsRssURL': the RSS feed URL. 
 function fetchSomeNewsWithJQuery(newsRssURL) 
 {
+    //use the google feeds API to obtain the RSS URL's contents.
     var feed = new google.feeds.Feed(newsRssURL);
     feed.load(function (result) {
+        //If we loaded the feed successfully, then display the feed elements!
         if (!result.error) {
-            //First, we should remove any existing news items that we already displayed. 
-            $('.news-item').remove(); 
+            //First, we should remove any existing news items that we already displayed
+            //Otherwise, we'll keep appending stuff to the list with each click. 
+            $('.news-item').remove();
+            $('.sports-item').remove();
             //For each feed entry, write the news item to the 'news' div. 
             $.each(result.feed.entries, writeNewsItem);
         }
     }); //End feed.load()
 }//end fetchSomeNewsWithJQuery
 
-
+//Each news item has the following attributes: 
+//
+//link:             a string with the URL to the full story. 
+//publishedDate:    a string consisting of the published Date. 
+//title:            the title of the story. 
+//contentSnippet:   a string with a brief snippet of the story's content. 
+//content:          a string containing the entirety of the story's contents. 
+//author:           a string containing the story's author. 
+//categories:       a list of strings containing possible category names. 
 function writeNewsItem(index, newsItem) {
-                $('<div/>', {html: newsItem.title}).appendTo('#news').addClass('news-item');            
+               var divElement =  $('<div/>', {html: newsItem.title}).appendTo('#news').addClass('news-item');
+               divElement.append($('<div/>', {html: newsItem.contentSnippet}).addClass('snippet-text'));
 }
